d815c03c9c2fa8c2ab462eebb220e12ab0d66cc2
Alicia Reeves <reevesalic@gmail.com>
2020-06-07 19:32:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
    "Fizz"
-  elseif int % 5 == 0
+  else int % 5 == 0
    "Buzz"
-  else int % 3 && % 5 == 0
+  elseif int % 3 && % 5 == 0
    "FizzBuzz"
   #else "nil"
   end
